6ded8002288a02f9bc009bf1b019fe55d646476f
ShakibulDeveloper <epiktheme@gmail.com>
2023-12-23 21:44:37 +0600

    google map

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_google_map/ui/screens/home_screen.dart';
+import 'package:flutter_google_map/ui/screens/location.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
